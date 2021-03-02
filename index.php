06a6ec25737b3b410f9f061818e46a34cd107032
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-03-02 11:51:33 +0330

    create login form

--- index.php
+++ index.php
@@ -10,6 +10,33 @@
     <link rel="stylesheet" href="css/font.css">
 </head>
 <body>
+
+<div class="container mt-5 mb-2 mx-auto " style="max-width: 500px">
+<div class="card shadow">
+    <div class="card-header bg-white ">
+        <div class="text-center ">
+
+            <img src="img/tegarat_logo.png" class="" alt="">
+            <h4 class="persian text-primary">سامانه ثبت گزارشات و پیشنهادات</h4>
+        </div>
+    </div>
+    <div class="card-body">
+        <form action="">
+            <div class="form-group">
+                <label for="username" class="persian float-right">نام</label>
+                <input type="text" id="username" name="username"  class="form-control " >
+            </div>
+            <div class="form-group">
+                <label for="password" class="persian float-right">رمز عبور</label>
+                <input type="password" id="password" name="password"  class="form-control " >
+            </div>
+        </form>
+        <button type="submit" class="btn btn-primary persian">ورود</button>
+    </div>
+
+</div>
+</div>
+
 <script src="js/jquery-3.5.1.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 </body>
