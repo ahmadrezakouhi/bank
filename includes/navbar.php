b4f38fdd7e052087cb9fc739f6b1b539f206555f
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-03-02 13:27:00 +0330

    create navbar for user

---
+++ includes/navbar.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
+    <div class="dropdown">
+        <a class="navbar-brand  dropdown-toggle" href="#" data-toggle="dropdown">
+            <img src="../img/img_avatar1.png" class="img-fluid rounded-circle" alt="" style="width: 40px">
+        </a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item persian text-right text-danger" href="#">خروج</a>
+
+        </div>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link text-right" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-right" href="#">Link</a>
+            </li>
+
+        </ul>
+
+    </div>
+</nav>
