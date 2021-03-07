4729a53e27b9b94c450d138d026221f39f1555b2
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-03-07 22:04:09 +0330

    create navbar for admin

--- admin/index.php
+++ admin/index.php
@@ -6,11 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>login</title>
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/font.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font.css">
 </head>
 <body>
-
+<?php include "../includes/admin/navbar.php"?>
 <script src="js/jquery-3.5.1.min.js"></script>
 <script src="js/bootstrap.bundle.min.js"></script>
 </body>
--- includes/admin/navbar.php
+++ includes/admin/navbar.php
@@ -1,1 +1,27 @@
-<?php
+<nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
+    <div class="dropdown">
+        <a class="navbar-brand  dropdown-toggle" href="#" data-toggle="dropdown">
+            <img src="../img" class="img-fluid rounded-circle" alt="" style="width: 40px">
+        </a>
+        <div class="dropdown-menu">
+            <a class="dropdown-item persian text-right text-danger" href="#">خروج</a>
+
+        </div>
+    </div>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+
+            <li class="nav-item">
+                <a class="nav-link text-right persian" href="#">گزارش گیری</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link text-right persian" href="#">افزودن اعضا <span class="sr-only">(current)</span></a>
+            </li>
+        </ul>
+
+    </div>
+</nav>
