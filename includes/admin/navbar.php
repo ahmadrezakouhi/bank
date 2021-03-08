304d33baa3fbe9d54748e8613e4411b2efee1697
ahmad reza kouhi <ahmadrezakouhi@gmail.com>
2021-03-08 10:17:44 +0330

    added persian links and change color of navbar

--- includes/admin/navbar.php
+++ includes/admin/navbar.php
@@ -1,26 +1,26 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-white shadow">
+<nav class="navbar navbar-expand-lg navbar-dark bg-info shadow">
     <div class="dropdown">
         <a class="navbar-brand  dropdown-toggle" href="#" data-toggle="dropdown">
             <img src="../img" class="img-fluid rounded-circle" alt="" style="width: 40px">
         </a>
         <div class="dropdown-menu">
             <a class="dropdown-item persian text-right text-danger" href="#">خروج</a>
-
+            <a class="dropdown-item persian text-right " href="#">تغییر رمز</a>
         </div>
     </div>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
     </button>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-
-            <li class="nav-item">
+    <div class="collapse navbar-collapse  flex-row-reverse" id="collapsibleNavbar">
+        <ul class="navbar-nav ">
+            <li class="nav-item order-1 active">
+                <a class="nav-link text-right persian" href="#">افزودن کاربر <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item order-2">
                 <a class="nav-link text-right persian" href="#">گزارش گیری</a>
             </li>
-            <li class="nav-item active">
-                <a class="nav-link text-right persian" href="#">افزودن اعضا <span class="sr-only">(current)</span></a>
-            </li>
+
         </ul>
 
     </div>
